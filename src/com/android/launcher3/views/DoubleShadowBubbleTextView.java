cb20098e5a80ee087db9d038b3a19639065e156d
Shreyansh <gauravguptasggs@gmail.com>
2026-08-04 20:55:34 +0530

    fix: draw multi-select badges on workspace page app icons by overriding DoubleShadowBubbleTextView.onDraw

--- src/com/android/launcher3/views/DoubleShadowBubbleTextView.java
+++ src/com/android/launcher3/views/DoubleShadowBubbleTextView.java
@@ -133,6 +133,7 @@ public class DoubleShadowBubbleTextView extends BubbleTextView {
 
         drawDotIfNecessary(canvas);
         drawRunningAppIndicatorIfNecessary(canvas);
+        drawSelectionBadgeIfNecessary(canvas);
     }
 
     private boolean skipDoubleShadow() {
