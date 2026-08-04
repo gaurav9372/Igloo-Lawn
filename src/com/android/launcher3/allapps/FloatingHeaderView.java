318c757871ceab3e93fb52193fd2de83fe09f923
Shreyansh <gauravguptasggs@gmail.com>
2026-08-04 21:57:05 +0530

    fix: add safe mCurrentRV null guard in FloatingHeaderView.reset to prevent tab strip translation corruption

--- src/com/android/launcher3/allapps/FloatingHeaderView.java
+++ src/com/android/launcher3/allapps/FloatingHeaderView.java
@@ -405,7 +405,7 @@ public class FloatingHeaderView extends LinearLayout implements
         }
         mHeaderCollapsed = false;
         mSnappedScrolledY = -mMaxTranslation;
-        if (!PreferenceCacheExtensionsKt.firstCached(pref2.getRememberPosition())) {
+        if (mCurrentRV != null && !PreferenceCacheExtensionsKt.firstCached(pref2.getRememberPosition())) {
             mCurrentRV.scrollToTop();
         }
     }
