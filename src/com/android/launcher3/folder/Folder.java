298e4e33b19e4f42491b19cd9a7cd1a0fb612367
Shreyansh <gauravguptasggs@gmail.com>
2026-08-07 01:04:40 +0530

    Use fully qualified name for LauncherSettings in Folder.java

--- src/com/android/launcher3/folder/Folder.java
+++ src/com/android/launcher3/folder/Folder.java
@@ -528,7 +528,7 @@ public class Folder extends AbstractFloatingView implements ClipPathView, DragSo
     }
 
     public boolean isInAppDrawer() {
-        return mInfo.container < 0 || mInfo.container == ItemInfo.NO_ID || mInfo.container == LauncherSettings.Favorites.CONTAINER_ALL_APPS;
+        return mInfo.container < 0 || mInfo.container == ItemInfo.NO_ID || mInfo.container == com.android.launcher3.LauncherSettings.Favorites.CONTAINER_ALL_APPS;
     }
 
     @Override
